7adb13ccde7f0d9933be4f9a05d8f2355ac3e0e3
Hai Tran <vroyibg@gmail.com>
2021-11-24 22:26:23 +0700

    fix json

--- get_languages/main.py
+++ get_languages/main.py
@@ -1,9 +1,11 @@
+import json
+
 from google.cloud import firestore
 
 def get_languages(request):
     db = firestore.Client()
-    texts_ref = db.collection(u'languages')
+    languages_ref = db.collection(u'languages')
     rs = []
-    for doc in texts_ref.stream():
+    for doc in languages_ref.stream():
         rs.append(doc)
-    return rs
+    return json.dumps(rs)
