61b31e0d9dd632d7b487a80e64b4fc3115b8dd69
Hai Tran <vroyibg@gmail.com>
2021-11-25 00:22:56 +0700

    create decorator

--- get_languages/main.py
+++ get_languages/main.py
@@ -1,9 +1,32 @@
 from google.cloud import firestore
-from flask_cors import cross_origin
 from flask import jsonify
 
 
-@cross_origin(allowed_methods=['GET'], origins=["http://localhost", "https://haitran.dev"])
+def cross_origin(allowed_methods="*", allowed_origins="*", allowed_headers="*"):
+    def get_header_attr_value(value):
+        if type(value) is str:
+            return value
+        if type(value) is list:
+            return ",".join(value)
+        raise Exception("value type not supported")
+
+    def decorator(handler):
+        def wrapped(request):
+            if request.method == 'OPTIONS':
+                headers = {
+                    'Access-Control-Allow-Origin': get_header_attr_value(allowed_origins),
+                    'Access-Control-Allow-Methods': get_header_attr_value(allowed_methods),
+                    'Access-Control-Allow-Headers': get_header_attr_value(allowed_headers),
+                }
+                return '', 204, headers
+            return handler(request)
+
+        return wrapped
+
+    return decorator
+
+
+@cross_origin(allowed_methods=['GET'], allowed_origins=["http://localhost", "https://haitran.dev"])
 def get_languages(request):
     db = firestore.Client()
     languages_ref = db.collection(u'languages')
