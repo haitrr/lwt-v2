c66d1aa9865cb4b816707f99f7700504230d9ab8
Hai Tran <vroyibg@gmail.com>
2021-11-24 23:44:28 +0700

    fix

--- get_languages/main.py
+++ get_languages/main.py
@@ -1,14 +1,13 @@
 from google.cloud import firestore
 from flask_cors import cross_origin
-from flask import json
+from flask import jsonify
 
 
 @cross_origin(allowed_methods=['GET'], origins=["http://localhost", "https://haitran.dev"])
-@json
 def get_languages(request):
     db = firestore.Client()
     languages_ref = db.collection(u'languages')
     rs = []
     for doc in languages_ref.stream():
         rs.append(doc.to_dict())
-    return json.dumps(rs)
+    return jsonify(rs)
