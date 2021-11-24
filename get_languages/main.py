45cc1c6b6d84615f11f0e2c871438eaeb5b3bb40
Hai Tran <vroyibg@gmail.com>
2021-11-24 22:19:10 +0700

    get languages

--- get_languages/main.py
+++ get_languages/main.py
@@ -1,16 +1,9 @@
+from google.cloud import firestore
+
 def get_languages(request):
-    """Responds to any HTTP request.
-    Args:
-        request (flask.Request): HTTP request object.
-    Returns:
-        The response text or any set of values that can be turned into a
-        Response object using
-        `make_response <http://flask.pocoo.org/docs/1.0/api/#flask.Flask.make_response>`.
-    """
-    request_json = request.get_json()
-    if request.args and 'message' in request.args:
-        return request.args.get('message')
-    elif request_json and 'message' in request_json:
-        return request_json['message']
-    else:
-        return f'Hello World! Deploy 1!'
+    db = firestore.Client()
+    texts_ref = db.collection(u'languages')
+    rs = []
+    for doc in texts_ref.stream():
+        rs.append(doc)
+    return rs
